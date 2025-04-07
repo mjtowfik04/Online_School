c9f4d653022f698eba71ea03a62fbab5366ce271
towfik <mjtowfik659672@gmail.com>
2025-04-08 00:49:57 +0600

    done2

--- online_school/settings.py
+++ online_school/settings.py
@@ -183,3 +183,9 @@ cloudinary.config(
 )
 DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
