d50d3eff0881c98d4f132745e5688560ca417995
admin <mjtowfik659672@gmail.com>
2025-05-12 16:41:22 +0600

    error4

--- order/models.py
+++ order/models.py
@@ -19,7 +19,7 @@ class Cart(models.Model):
 class CartItem(models.Model):
     cart = models.ForeignKey(
         Cart, on_delete=models.CASCADE, related_name="items")
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Course, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
 
     class Meta:
