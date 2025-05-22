a38c988b630e08e93f2f6edabc1ec9a6caf726ae
admin <mjtowfik659672@gmail.com>
2025-05-23 01:02:06 +0600

    some change

--- order/models.py
+++ order/models.py
@@ -4,12 +4,9 @@ from django.conf import settings
 from courses.models import Course
 
 class Enroll(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='enrollments')
+    Email= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='enrollments')
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     enrolled_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        unique_together = ('user', 'course') 
-
     def __str__(self):
-        return f"{self.user.get_full_name() or self.user.username} enrolled in {self.course.title}"
+        return f"{ self.Email} enrolled in {self.course.title}"
--- order/serializers.py
+++ order/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 from .models import Enroll
 
 class EnrollSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True)  # ইউজারের নাম দেখাবে, রিড-ওনলি
+    Email = serializers.StringRelatedField(read_only=True)  # ইউজারের নাম দেখাবে, রিড-ওনলি
     course = serializers.StringRelatedField()  # কোর্সের টাইটেল দেখাবে
 
     class Meta:
         model = Enroll
-        fields = ['id', 'user', 'course', 'enrolled_at']
-        read_only_fields = ['id', 'user', 'enrolled_at']
+        fields = ['id', 'Email', 'course', 'enrolled_at']
+        read_only_fields = ['id', 'Email', 'enrolled_at']
